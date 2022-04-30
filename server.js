8d45dc681a58e19750d8168965d84bbe0d1e1f8d
Tim Sorrow <timursurov@gmail.com>
2022-04-30 17:09:22 +0600

    Fix 21 try 143

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+
 const register = require('./controllers/register')
 const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image');
-const { Db } = require('pg');
 
 const db = knex({
+      client: 'pg',
+      connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
+        ssl: {
+          rejectUnauthorized: false
+      }
     }
 });
 
