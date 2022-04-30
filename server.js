e0d4ffd8e2fffa59204cdc8d6323b299ac29245d
Tim Sorrow <timursurov@gmail.com>
2022-04-30 16:53:25 +0600

    Fix 17 try 143

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM users');
+    const result = await client.query('SELECT * FROM test_table');
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/db', results );
     client.release();
