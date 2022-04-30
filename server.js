18545d361d37a54034da10c443bdcadf509e00ff
Tim Sorrow <timursurov@gmail.com>
2022-04-30 18:18:25 +0600

    Fix 25 try 143

--- server.js
+++ server.js
@@ -1,50 +1,60 @@
-import express from 'express';
-import cors from 'cors';
-import knex from 'knex';
-import bcrypt from 'bcrypt-nodejs';
-import handleRegister from './controllers/register.js';
-import handleSignin from './controllers/signin.js';
-import handleProfileGet from './controllers/profile.js';
-import { handleApiCall, handleImage } from './controllers/image.js';
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  },
+	client: "pg",
+	connection: {
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	},
 });
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('success');
+app.get("/", (req, res) => {
+	res.send("it is working !!");
 });
 
-app.post('/signin', (req, res) => {
-  handleSignin(req, res, db, bcrypt);
-});
+app.post("/signin", signin.handleSignin(db, bcrypt)); // -> function will receive the db,bcrypt first and then receive req,res later as refer to the previous line of get '/'
 
-app.post('/register', (req, res) => {
-  handleRegister(req, res, db, bcrypt);
-});
+app.post("/register", (req, res) =>
+	register.handleRegister(req, res, db, bcrypt)
+);
 
-app.get('/profile/:id', (req, res) => {
-  handleProfileGet(req, res, db);
-});
+app.get("/profile/:id", (req, res) => profile.handleProfileGet(req, res, db));
 
-app.put('/image', (req, res) => {
-  handleImage(req, res, db);
-});
+app.put("/image", (req, res) => image.handleImage(req, res, db));
 
-app.post('/imageurl', (req, res) => {
-  handleApiCall(req, res);
-});
+app.post("/imageurl", (req, res) => image.handleAPICall(req, res));
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
+	console.log(`app is running on port ${process.env.PORT}`);
 });
