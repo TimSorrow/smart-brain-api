5c1f0d24a2148a3f1229455489ca94c6943a693b
Tim Sorrow <timursurov@gmail.com>
2022-04-30 12:39:58 +0600

    Fix 14 try 143

--- server.js
+++ server.js
@@ -24,7 +24,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('it is working');})
+//app.get('/', (req, res) => {res.send('it is working');})
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', register.handleRegister(db, bcrypt))
 app.get('/profile', profile.handleProfileGet(db))
