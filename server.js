b9f7a2cec6c583604a591121790d9dfbd26ac95c
Tim Sorrow <timursurov@gmail.com>
2022-05-03 00:10:04 +0600

    Fix 24 exp port connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const { db } = require('pg');
+const { Pool } = require('pg');
 const { parse } = require('pg-connection-string')
 
 
