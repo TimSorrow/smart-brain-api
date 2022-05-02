4aa05b3fb5ea46247be317ddba7eed9f8a49dca5
Tim Sorrow <timursurov@gmail.com>
2022-05-03 00:02:01 +0600

    Fix 22 exp port connection

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const { db } = require('pg');
+
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
