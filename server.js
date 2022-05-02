6eaf42a38c5aa3b74d3fae3a84016b3f948f696c
Tim Sorrow <timursurov@gmail.com>
2022-05-02 14:14:39 +0600

    Fix 12 exp port connection

--- server.js
+++ server.js
@@ -10,16 +10,38 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const { ssl } = require('pg/lib/defaults');
 
+import knex, { Config } from "knex";
+import knexStringcase from "knex-stringcase";
+import { db, isDev } from "../config";
+
+
 //process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
-const db = knex({
+//const db = knex({
 	// connect to your own database here:
-	client: 'pg',
-	connection:  {
-		connectionString : process.env.DATABASE_URL,
-		ssl: false
-	  }
-  });
+//	client: 'pg',
+//	connection:  {
+//		connectionString : process.env.DATABASE_URL,
+//		ssl: false
+//	  }
+//  });
+
+const config = knexStringcase({
+	debug: isDev,
+	client: "pg",
+	connection: {
+	  connectionString: db,
+	  ssl: {
+		rejectUnauthorized: false,
+	  },
+	},
+	searchPath: ["public"],
+	// pool: isDev ? { max: 4 } : { min: 4 },
+	asyncStackTraces: true,
+  } as Config) as Config;
+  
+
+
 
 const app = express();
 
@@ -36,3 +58,5 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 app.listen(process.env.PORT || 3000, ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
 })
+
+export default knex(config);
